8853abc0f35a87834da12474972e35eee1372aeb
konyan <devkonyan@gmail.com>
2022-04-20 22:31:43 +0630

    dynamic generate alert

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,33 +1,77 @@
-import { useContext } from 'react';
+import { useContext, useReducer, useState } from 'react';
 import { AlertContext } from '../alert/AlertProvider';
+
+const formReducer = (state, event) => {
+  return {
+    ...state,
+    [event.target.name]: event.target.value,
+  };
+};
 
 const Button = () => {
   const dispatch = useContext(AlertContext);
+  const [formData, setFormData] = useReducer(formReducer, {});
+
   console.log('CONTEXT', dispatch);
 
   const addAlert = (type) => {
+    console.log('FORM DATA', formData);
+
     dispatch({
       type: 'ADD_ALERT',
-      payload: {
-        title: 'TITLE',
-        message: 'Hello World',
-        type: type,
-        timeLimit: 10,
-      },
+      payload: { ...formData },
     });
   };
+
   return (
     <div>
-      <button onClick={(e) => addAlert('success')} className='btn btn-success'>
-        Click Me Alert
-      </button>
-      <button onClick={(e) => addAlert('error')} className='btn btn-error'>
-        Click Me Alert
-      </button>
-      <button onClick={(e) => addAlert('info')} className='btn btn-info'>
-        Click Me Alert
-      </button>
-      <button onClick={(e) => addAlert('warning')} className='btn btn-warning'>
+      <label htmlFor='title'>Enter Title</label>
+      <input
+        type='text'
+        value={formData.title || ''}
+        name='title'
+        className='input'
+        onChange={setFormData}
+      />
+      <label htmlFor='message'>Enter Message</label>
+      <input
+        type='text'
+        value={formData.message || ''}
+        name='message'
+        className='input'
+        onChange={setFormData}
+      />
+      <label htmlFor='type'>Alert Type</label>
+      <br />
+      <select
+        name='type'
+        id='type'
+        className='select'
+        value={formData.type || ''}
+        onChange={setFormData}
+      >
+        <option value='error'>Error</option>
+        <option value='info'>Info</option>
+        <option value='warning'>Warning</option>
+        <option value='success'>Success</option>
+      </select>
+      <br />
+      <label htmlFor='timeLimit'>Alert Seconds</label>
+      <br />
+      <select
+        name='timeLimit'
+        id='timeLimit'
+        className='select'
+        onChange={setFormData}
+        value={formData.timeLimit || ''}
+      >
+        <option value='4'>4 seconds</option>
+        <option value='5'>5 seconds</option>
+        <option value='6'>6 seconds</option>
+        <option value='8'>8 seconds</option>
+        <option value='10'>10 seconds</option>
+      </select>
+      <button onClick={(e) => addAlert()} className='btn btn-success'>
         Click Me Alert
       </button>
     </div>
